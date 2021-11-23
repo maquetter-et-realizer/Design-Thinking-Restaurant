cc987759a85902fd77a0342264949d61ed1a82bb
elabbasyounes <92955866+elabbasyounes@users.noreply.github.com>
2021-11-23 16:58:49 +0100

    Add files via upload

---
+++ menu.js
@@ -0,0 +1,9 @@
+function menu(){
+    var x = document.getElementById("navlist");
+    if(x.style.display === "block"){
+        x.style.display= "none";
+    } else{
+        x.style.display = "block";
+    }
+
+}
